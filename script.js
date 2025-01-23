9bad9f711ea63222bf07d9020814d1039755f254
nicolamarino <nicolamarino527@gmail.com>
2025-01-23 10:46:21 +0100

    add script function and style

--- script.js
+++ script.js
@@ -6,3 +6,27 @@
 // Bonus
 // Abbellire con CSS o Bootstrap
 // Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
+// scriviamo le varibili input
+const endPoint = `https://flynn.boolean.careers/exercises/api/random/mail`;
+const emailList = document.getElementById('email-list');
+let emailNumber = 2;
+
+
+// creare una funzione per richiamare un a mail tramie  axios ad un api
+function giveEmail() {
+    for (let i = 0; i < emailNumber; i++) {
+        axios.get(endPoint) 
+        // se la risposta è p ositiva
+        .then(response => {
+            const result = response.data.response; 
+            emailList.innerHTML += `<li>${result}</li>`;
+        })
+        // se la risposta è negativa
+        .catch(error => {
+            console.error(error); 
+        });
+    }
+};
+    
+giveEmail();
